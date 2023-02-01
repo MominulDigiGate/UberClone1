3b2891d4cfdfa4bea977d3c0ee4f7e2a107fc95a
Mominul <mominul.islam@digigate30.com>
2023-02-01 17:35:23 +0600

    zoom out polygon on location selcted

--- UberClone/Core/Views/UberMapViewRepresentable.swift
+++ UberClone/Core/Views/UberMapViewRepresentable.swift
@@ -92,7 +92,7 @@ extension UberMapViewRepresentable
             anno.coordinate = coordinate
             parent.mapView.addAnnotation(anno)
             parent.mapView.selectAnnotation(anno, animated: true)
-            parent.mapView.showAnnotations(parent.mapView.annotations, animated: true)
+//            parent.mapView.showAnnotations(parent.mapView.annotations, animated: true)
         }
         
         func configPolyline( withDestinationCoordinate coordinate: CLLocationCoordinate2D){
@@ -100,6 +100,11 @@ extension UberMapViewRepresentable
             
             getDestinationRoute(from: userLocationCoordinate, to: coordinate){ route in
                 self.parent.mapView.addOverlay(route.polyline)
+                let rect = self.parent.mapView.mapRectThatFits(route.polyline.boundingMapRect,
+                                                               edgePadding: .init(top: 64, left: 32, bottom: 500, right: 32))
+                
+                self.parent.mapView.setRegion(MKCoordinateRegion(rect), animated: true)
+                
             }
         }
         
