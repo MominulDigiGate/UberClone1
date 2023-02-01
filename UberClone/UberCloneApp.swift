0f780b4eeeb99d97ecdb268e624f8373e6dd7a36
Mominul <mominul.islam@digigate30.com>
2023-02-01 11:21:54 +0600

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Mac Mini 5 on 1/2/23.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
